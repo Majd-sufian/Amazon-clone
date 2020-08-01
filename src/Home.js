6faf0b739760565e8060855e0d75d184f92370bf
Majd Sufian <majdnewsufian@gmail.com>
2020-08-01 23:18:09 +0200

    creating the header and the banner

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Beauty_v2_en_US_1x._CB429089975_.jpg"
+        alt="amazon Banner"
+      />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+// // https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg
+// https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Projects/Skala/Israel/Fuji_Hero_SkalaFinal5_1X_en_US._CB452377449_.jpg
+// https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg
